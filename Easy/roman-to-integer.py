992b112fca404d0c30fe3aeb390d021ca8b70eda
Nicolas Lara Fonseca <53790517+larafonse@users.noreply.github.com>
2023-02-12 10:29:19 -0800

    Map Solution added to roman-to-integer problem

--- Easy/roman-to-integer.py
+++ Easy/roman-to-integer.py
@@ -48,3 +48,35 @@ class Solution:
                 result+=1000
             i+=1
         return result
+
+# Using map
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        romanToNum = {
+            'I':1,
+            'V':5,
+            'X':10,
+            'L':50,
+            'C':100,
+            'D':500,
+            'M':1000,
+            'IV':4,
+            'IX':9,
+            'XL':40,
+            'XC':90,
+            'CD':400,
+            'CM':900,
+        }
+        i = 0
+        result = 0
+
+        while i<len(s):
+            if i == len(s)-1:
+                result += romanToNum[s[i]]
+            elif (s[i]+s[i+1]) in romanToNum:
+                result+=romanToNum[s[i]+s[i+1]]
+                i+=1
+            else:
+                result+=romanToNum[s[i]]
+            i+=1
+        return result
